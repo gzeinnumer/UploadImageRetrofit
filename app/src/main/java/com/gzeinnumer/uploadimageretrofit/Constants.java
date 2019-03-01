178508e449239a81134b043dec1a03f42dffe124
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2019-03-01 18:54:35 +0700

    Initial commit

---
+++ app/src/main/java/com/gzeinnumer/uploadimageretrofit/Constants.java
@@ -0,0 +1,6 @@
+package com.gzeinnumer.uploadimageretrofit;
+
+public class Constants {
+    public static final String BASE_URL = "https://yourhost/exer_oploadfile_multipart/" ;
+
+}
---
+++ app/src/main/java/com/gzeinnumer/uploadimageretrofit/model/ResponseApiModel.java
@@ -0,0 +1,30 @@
+package com.gzeinnumer.uploadimageretrofit.model;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by hakiki95 on 4/26/2017.
+ */
+
+public class ResponseApiModel {
+    @SerializedName("kode")
+    String kode;
+    @SerializedName("pesan")
+    String pesan;
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getPesan() {
+        return pesan;
+    }
+
+    public void setPesan(String pesan) {
+        this.pesan = pesan;
+    }
+}
